46585eef2b9a80dbcb1dfe1c3338eb05e4fd679c
Ryan Tan <ryantan.mail@gmail.com>
2025-03-13 14:18:00 -0700

    wifi esp8266

--- arduino_src/ESP8266/ESP8266.ino
+++ arduino_src/ESP8266/ESP8266.ino
@@ -1,6 +1,9 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 
-const char* wifi_ssid = "SJSU_device";
+const char* wifi_ssid = "REPLACE";
+const char* wifi_password = "REPLACE HERE";
+const String get_ping_url = "http://IP/ping"; //
 
 void setup()
 {
@@ -13,10 +16,37 @@ void setup()
   }
   Serial.println();
 
-  Serial.print("Connected, IP address: ");
+  Serial.println("Connected, IP address: ");
   Serial.println(WiFi.localIP());
   Serial.println(WiFi.SSID());
+  Serial.println();
 }
 
 void loop() {
+
+  if(WiFi.status() != WL_CONNECTED) {
+    Serial.println("Not connected to a WiFi network");
+    return;
+  }
+
+  WiFiClient wifi_client;
+  HTTPClient http;
+
+  http.begin(wifi_client, get_ping_url);
+
+  int http_response_code = http.GET();
+  if (http_response_code <= 0) {
+    Serial.print("HTTP Request to ping failed");
+    Serial.println(http_response_code);
+  } 
+  
+  Serial.println("HTTP Response code: ");
+  Serial.println(http_response_code);
+  String response = http.getString();
+  Serial.println("Response body: ");
+  Serial.println(response);
+  http.end();
+
+  
+  delay(5000);
 }
