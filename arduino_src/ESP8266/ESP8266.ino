809104c0227d7fbfc21b093b02d9d59830787b51
Ryan Tan <ryantan.mail@gmail.com>
2025-03-27 16:36:49 -0700

    prototype data send

--- arduino_src/ESP8266/ESP8266.ino
+++ arduino_src/ESP8266/ESP8266.ino
@@ -1,52 +1,72 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-
-const char* wifi_ssid = "REPLACE";
-const char* wifi_password = "REPLACE HERE";
-const String get_ping_url = "http://IP/ping"; //
+#include "WiFiCredential.h"
+#include <ArduinoJson.h>
 
 void setup()
 {
+  
   Serial.begin(9600);
-  Serial.println();
-  WiFi.begin(wifi_ssid);
+  //Serial.println();
+  WiFi.begin(wifi_ssid, wifi_password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");    
+    //Serial.print(".");    
   }
-  Serial.println();
+  //Serial.println();
 
-  Serial.println("Connected, IP address: ");
-  Serial.println(WiFi.localIP());
-  Serial.println(WiFi.SSID());
-  Serial.println();
+  //Serial.println("Connected, IP address: ");
+  //Serial.println(WiFi.localIP());
+  //Serial.println(WiFi.SSID());
+  //Serial.println();
 }
 
 void loop() {
 
   if(WiFi.status() != WL_CONNECTED) {
-    Serial.println("Not connected to a WiFi network");
+    //Serial.println("Not connected to a WiFi network");
     return;
   }
-
   WiFiClient wifi_client;
   HTTPClient http;
 
-  http.begin(wifi_client, get_ping_url);
+  http.begin(wifi_client, post_ping_url);
+  http.addHeader("Content-Type", "application/json");
 
-  int http_response_code = http.GET();
-  if (http_response_code <= 0) {
-    Serial.print("HTTP Request to ping failed");
-    Serial.println(http_response_code);
-  } 
+  //Sending request
+  DynamicJsonDocument doc(1024);
+  doc["type"] = "request";
+  serializeJson(doc,Serial);
+  Serial.println();
+  boolean messageReady = false;
+  String message = "";
+  while(messageReady == false) { // blocking but that's ok
+    if(Serial.available()) {
+      message = Serial.readString();
+      messageReady = true;
+    }
+  }
+  DeserializationError error = deserializeJson(doc,message);
+  if(error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.c_str());
+    return;
+  }
   
-  Serial.println("HTTP Response code: ");
-  Serial.println(http_response_code);
-  String response = http.getString();
-  Serial.println("Response body: ");
-  Serial.println(response);
-  http.end();
+  //Retrieval of information
+  StaticJsonDocument<200> body_doc;
+  body_doc["message"] = doc["humidity"];
+  String requestBody;
+  serializeJson(body_doc, requestBody);
 
-  
-  delay(5000);
+  // Send POST request
+  int httpResponseCode = http.POST(requestBody);
+        
+  if (httpResponseCode > 0) {
+    String response = http.getString();
+    //Serial.println("Response: " + response);
+  } else {
+    //Serial.println("Error on HTTP request");
+  }
+  delay(10000);
 }
--- arduino_src/soil_moister_sensor/soil_moister_sensor.ino
+++ arduino_src/soil_moister_sensor/soil_moister_sensor.ino
@@ -1,18 +1,48 @@
+#include <ArduinoJson.h>
 
 const int wet = 203;
 const int dry = 456;
+String message = "";
+bool messageReady = false;
 
 void setup()
 {
   Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 // main loop
 void loop()
 {
-  int soilSensorVal = analogRead(A0);
-  int percentageHumidity = map(soilSensorVal, wet, dry, 100, 0);
-  Serial.print(percentageHumidity);
-  Serial.println("%");
-  delay(1000);
+  while(Serial.available()) {
+    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+    delay(1000);                      // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+    delay(1000);  
+    message = Serial.readString();
+    messageReady = true;
+  }
+  if(messageReady) {
+    // The only messages we'll parse will be formatted in JSON
+    DynamicJsonDocument doc(1024); // ArduinoJson version 6+
+    // Attempt to deserialize the message
+    
+    DeserializationError error = deserializeJson(doc,message);
+    if(error) {
+      Serial.print(message);
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.c_str());
+      messageReady = false;
+      return;
+    }
+    if(doc["type"] == "request") {
+      doc["type"] = "response";
+      // Get data from analog sensors
+      int soilSensorVal = analogRead(A0);
+      int percentageHumidity = map(soilSensorVal, wet, dry, 100, 0);
+      doc["humidity"] = String(percentageHumidity) + "%";
+      serializeJson(doc,Serial);
+    }
+    messageReady = false;
+  }
 }
